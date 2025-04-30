c6b59aaf5d989bc94608b19e13f7da7448452c0a
Walter Cabral <107414052+CabralWalter@users.noreply.github.com>
2025-04-30 08:15:15 -0300

    03

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./**/*.html", "./assets/js/**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#001042",
+        dark: "#101010",
+        accent: "#648DE5",
+        light: "#FFFFFF",
+        lavender: "#A675A1", // opcional
+      },
+    },
+  },
+  plugins: [],
+}
